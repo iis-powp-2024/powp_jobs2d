67b150c738727c5620b4d7d1b93722f436689b16
240844 <240844@edu.p.lodz.pl>
2024-02-29 13:41:44 +0100

    Fixed drawing on different windows.

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
@@ -4,15 +4,18 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature; // remove this
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class DrawerAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	private final DrawPanelController drawPanelController;
 
-	public DrawerAdapter() {
+	public DrawerAdapter() { // dodaj tutaj DrawPanelController
 		super();
+		this.drawPanelController = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -27,7 +30,7 @@ public class DrawerAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(startX, startY);
 		line.setEndCoordinates(x, y);
 		this.setPosition(x, y);
-		drawLine(line);
+		this.drawPanelController.drawLine(line); // fix this
 	}
 
 	@Override
