056c1ab5258044ddbf32e02de4c17f59c71da90c
Neras <arkadiusz.salaj.02@gmail.com>
2024-02-29 13:17:04 +0100

    Renaming adapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AdapterBoxDraw.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AdapterBoxDraw.java
@@ -8,10 +8,10 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class AdapterBoxDraw extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public AdapterBoxDraw() {
 		super();
 	}
 
@@ -26,7 +26,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
+		this.setPosition(x, y);
 		drawLine(line);
 	}
 
