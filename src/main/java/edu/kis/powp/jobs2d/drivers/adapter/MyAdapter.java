c7b7a727cd7a8bbc70c620d15a3778b65a6b1e28
Lecrut <filip.lecrut12@gmail.com>
2024-02-29 11:17:00 +0100

    poprawne ustawienie głowicy na koniec ruchu

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -27,6 +27,8 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
+		setPosition(x, y);
+
 		drawLine(line);
 	}
 
