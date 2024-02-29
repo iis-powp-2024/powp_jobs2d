6c4207b0c93a87410fab0c23369fd67383867e32
Eryk Skonieczny <240796@edu.p.lodz.pl>
2024-02-29 19:51:58 +0100

    Umożliwienie klientowi korzystania z funkcjonalności klasy DrawPanelController

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverAdapter.java
@@ -4,6 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -28,7 +29,8 @@ public class DriverAdapter extends DrawPanelController implements Job2dDriver {
 		line.setEndCoordinates(x, y);
 		this.setPosition(x, y);
 
-		drawLine(line);
+		DrawPanelController drawer = DrawerFeature.getDrawerController();
+		drawer.drawLine(line);
 	}
 
 	@Override
