67e34d0d37b4f109f1e84479a5276a3a88e56682
unknown <240763@edu.p.lodz.pl>
2024-02-29 11:39:56 +0100

    Przeniesiono rysowanie wizualizacji do głównego okna z okna Drawer

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverDrawer.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverDrawer.java
@@ -4,6 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -29,7 +30,8 @@ public class DriverDrawer extends DrawPanelController implements Job2dDriver {
 		startX = x;
 		startY = y;
 
-		drawLine(line);
+		DrawPanelController drawer = DrawerFeature.getDrawerController();
+		drawer.drawLine(line);
 	}
 
 	@Override
