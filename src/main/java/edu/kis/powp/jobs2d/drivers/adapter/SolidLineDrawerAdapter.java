19c284ec7090cf537dc892d68cdc720e1e7b841e
zurson <matu.jurek@gmail.com>
2024-03-06 19:51:36 +0100

    Removed unnecessary inheritance from SolidLineDrawerAdapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/SolidLineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/SolidLineDrawerAdapter.java
@@ -1,6 +1,5 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
@@ -10,31 +9,31 @@ import static edu.kis.powp.jobs2d.features.DrawerFeature.getDrawerController;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class SolidLineDrawerAdapter extends DrawPanelController implements Job2dDriver {
-	private int startX = 0, startY = 0;
+public class SolidLineDrawerAdapter implements Job2dDriver {
+    private int startX = 0, startY = 0;
 
-	public SolidLineDrawerAdapter() {
-		super();
-	}
+    public SolidLineDrawerAdapter() {
 
-	@Override
-	public void setPosition(int x, int y) {
-		this.startX = x;
-		this.startY = y;
-	}
+    }
 
-	@Override
-	public void operateTo(int x, int y) {
-		ILine line = LineFactory.getBasicLine();
-		line.setStartCoordinates(this.startX, this.startY);
-		line.setEndCoordinates(x, y);
-		setPosition(x, y);
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
 
-		getDrawerController().drawLine(line);
-	}
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line = LineFactory.getBasicLine();
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+        setPosition(x, y);
 
-	@Override
-	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
-	}
+        getDrawerController().drawLine(line);
+    }
+
+    @Override
+    public String toString() {
+        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+    }
 }
