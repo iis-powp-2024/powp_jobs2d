c0f10bf1de710aeae278e7e2bd9f7a22422d0525
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-03-06 19:35:27 +0100

    Removing unnecessary inheritance from LineDrawerAdapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -3,14 +3,16 @@ package edu.kis.powp.jobs2d.drivers.adapter;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
-import edu.kis.legacy.drawer.shape.line.SpecialLine;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 import static edu.kis.powp.jobs2d.features.DrawerFeature.getDrawerController;
 
-public class LineDrawerAdapter extends DrawPanelController implements Job2dDriver {
+public class LineDrawerAdapter implements Job2dDriver {
     private int startX = 0, startY = 0;
 
+    public LineDrawerAdapter() {
+
+    }
     @Override
     public void setPosition(int x, int y) {
         this.startX = x;
