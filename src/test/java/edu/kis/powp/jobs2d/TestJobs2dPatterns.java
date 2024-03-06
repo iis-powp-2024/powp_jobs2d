82c0b27de663a9b3b79fe46f4687e39d07e8f1e1
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-03-06 19:29:03 +0100

    dodanie rysowania przykładów za pomocą SpecialLine

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -10,6 +10,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawingAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigure2OptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
@@ -48,6 +49,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new DrawingAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver specialLineDriver = new LineDrawerAdapter();
+		DriverFeature.addDriver("Special Line Drawer", specialLineDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
