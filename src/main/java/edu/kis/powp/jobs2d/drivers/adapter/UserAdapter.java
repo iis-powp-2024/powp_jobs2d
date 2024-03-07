93029e9d5c931bc36798ea9f4a921b5b619816bb
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2024-03-07 13:40:15 +0100

    fix drawing

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/UserAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/UserAdapter.java
@@ -26,6 +26,7 @@ public class UserAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
+		setPosition(x, y);
 
 		drawLine(line);
 	}
