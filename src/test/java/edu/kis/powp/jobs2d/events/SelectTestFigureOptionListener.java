1d157a87d6f31ae95d961a69ea2bfa8be36b66b8
Artur Trębski <trebski.artur@gmail.com>
2024-03-07 20:18:11 +0100

    Formatting all files for estetics

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -8,23 +8,23 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
-	private int testNo;
+    private DriverManager driverManager;
+    private int testNo;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager, int testNo) {
-		this.driverManager = driverManager;
-		this.testNo = testNo;
-	}
+    public SelectTestFigureOptionListener(DriverManager driverManager, int testNo) {
+        this.driverManager = driverManager;
+        this.testNo = testNo;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		switch (testNo){
-			case(1):
-				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-				break;
-			case(2):
-				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-				break;
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (testNo) {
+            case (1):
+                FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+                break;
+            case (2):
+                FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+                break;
+        }
+    }
 }
