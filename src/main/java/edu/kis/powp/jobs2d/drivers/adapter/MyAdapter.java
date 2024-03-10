d4b5727ec51e8e514a79f6ae345fd75d184a109a
Olek Golus <olekgolus02@gmail.com>
2024-03-10 21:16:18 +0100

    Change figure

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -27,6 +27,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
+		setPosition(x,y);
 
 		DrawerFeature.getDrawerController().drawLine(line);
 	}
