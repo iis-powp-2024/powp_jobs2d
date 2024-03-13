2b40506f2b6fa6343a31ec9588a06716c02b13ce
juliusz brodnicki <juliusbrodnicki@gmail.com>
2024-03-14 00:23:19 +0100

    Added "Figure Joe 2" test

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -13,6 +13,7 @@ import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class TestJobs2dPatterns {
 	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -26,7 +27,16 @@ public class TestJobs2dPatterns {
 		SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener(
 				DriverFeature.getDriverManager());
 
+		SelectTestFigureOptionListener selectTestFigureOptionListener2 =
+				new SelectTestFigureOptionListener(DriverFeature.getDriverManager()){
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						FiguresJoe.figureScript2(DriverFeature.getDriverManager().getCurrentDriver());
+					}
+				};
+
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		application.addTest("Figure Joe 2", selectTestFigureOptionListener2);
 	}
 
 	/**
