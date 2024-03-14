c7c7a3bb78e1f412391122fee79b445723fc7cd2
mwteusz <240759@edu.p.lodz.pl>
2024-03-14 13:45:50 +0100

    fix for recrangle

--- src/main/java/edu/kis/powp/command/CommandFactory.java
+++ src/main/java/edu/kis/powp/command/CommandFactory.java
@@ -4,6 +4,7 @@ public class CommandFactory {
 
     public static ComplexCommand createRectangle(int x, int y, int width, int height){
         ComplexCommand complexCommand = new ComplexCommand();
+        complexCommand.addCommand(new SetPositionCommand(x, y));
         complexCommand.addCommand(new OperateToCommand(x + width, y));
         complexCommand.addCommand(new OperateToCommand(x + width, y + height));
         complexCommand.addCommand(new OperateToCommand( x, y + height));
