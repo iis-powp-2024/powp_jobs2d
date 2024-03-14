7bc69b69326d72bf684f0564a6cc17fa953e7302
Matx1002 <35429241+Matx1002@users.noreply.github.com>
2024-03-14 23:44:53 +0100

    Remove unnecessary imports in LiveDrawerAdapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -2,10 +2,7 @@ package edu.kis.powp.jobs2d.drivers.adapter;
 
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
-import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-
-import java.util.function.Supplier;
 
 public class LineDrawerAdapter implements Job2dDriver {
     private int startX = 0, startY = 0;
