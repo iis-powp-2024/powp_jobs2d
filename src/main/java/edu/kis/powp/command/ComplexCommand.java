39a90a83d3138a95851acb954c0437eae4740c56
DM <240752@edu.p.lodz.pl>
2024-03-14 11:37:04 +0100

    Wprowadzenie klasy ComplexCommand

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.command;
+
+public class ComplexCommand implements DriverCommand{
+    //implementuje DriverCommand i agregacja ComplexCommand do 0..* DriverCommand
+    public void execute() {
+
+    }
+}
