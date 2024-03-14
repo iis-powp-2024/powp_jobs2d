970c9261f5095ceea2fa3699010893fb9ffd76b0
Yibux <240810@edu.p.lodz.pl>
2024-03-14 11:38:19 +0100

    changing ArrayList to List

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommand.java
@@ -3,10 +3,11 @@ package edu.kis.powp.jobs2d.command;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ComplexCommand implements DriverCommand{
 
-    private final ArrayList<DriverCommand> commands;
+    private final List<DriverCommand> commands;
 
     public ComplexCommand() {
         this.commands = new ArrayList<>();
