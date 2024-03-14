6c07e2d42b5042d0362d229e5b4cb6a9f9590937
Aveniss <kamilst934@gmail.com>
2024-03-14 11:35:40 +0100

    Adding ComplexCommand

---
+++ src/main/java/command/ComplexCommand.java
@@ -0,0 +1,25 @@
+package command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.interfaces.DriverCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+    private final List<DriverCommand> commands;
+
+    public ComplexCommand(){
+        commands = new ArrayList<>();
+    }
+
+    public void addCommand(DriverCommand command){
+        commands.add(command);
+    }
+    @Override
+    public void execute(Job2dDriver driver) {
+        for (DriverCommand command : commands) {
+            command.execute(driver);
+        }
+    }
+}
