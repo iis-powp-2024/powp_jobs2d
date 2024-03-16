4bb86965065c2b9da7daa52fca6b9440ad129f8c
KacperM-02 <240751@edu.p.lodz.pl>
2024-03-16 23:23:16 +0100

    Dodanie test, aby moc pokazac drugi przykladowy wzorzec

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -8,14 +8,24 @@ import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+	private final DriverManager driverManager;
+	int figuresJoeScriptNumber;
 
-	public SelectTestFigureOptionListener(DriverManager driverManager) {
+
+	public SelectTestFigureOptionListener(DriverManager driverManager, int figuresJoeScriptNumber) {
 		this.driverManager = driverManager;
+		this.figuresJoeScriptNumber = figuresJoeScriptNumber;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		if(this.figuresJoeScriptNumber == 1)
+		{
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		}
+		else
+		{
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
 	}
 }
