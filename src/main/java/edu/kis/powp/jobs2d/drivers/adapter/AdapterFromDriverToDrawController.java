618884d4a52f922293c0b6427bea2b7184b9ef34
KacperM-02 <240751@edu.p.lodz.pl>
2024-03-16 22:28:16 +0100

    Zmiana na precyzyjna nazwe klasy

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AdapterFromDriverToDrawController.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/AdapterFromDriverToDrawController.java
@@ -1,6 +1,5 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
@@ -10,10 +9,10 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class AdapterFromDriverToDrawController extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public AdapterFromDriverToDrawController() {
 		super();
 	}
 
