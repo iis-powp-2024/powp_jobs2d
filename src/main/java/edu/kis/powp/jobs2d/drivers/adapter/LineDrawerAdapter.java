7f0fb11984efd9835514b82c400a61e6f4297449
KacperM-02 <240751@edu.p.lodz.pl>
2024-03-17 16:18:16 +0100

    Zaimplementowanie wlasnego LineDrawerAdapter

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -0,0 +1,37 @@
+package edu.kis.powp.jobs2d.drivers.adapter;
+
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+/**
+ * Line drawer adapter
+ */
+public class LineDrawerAdapter implements Job2dDriver {
+    private int startX = 0, startY = 0;
+    private final ILine line;
+
+    public LineDrawerAdapter(ILine line) {
+        this.line = line;
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        this.line.setStartCoordinates(this.startX, this.startY);
+        this.line.setEndCoordinates(x, y);
+        this.setPosition(x, y);
+
+        DrawerFeature.getDrawerController().drawLine(line);
+    }
+
+    @Override
+    public String toString() {
+        return "Custom line driver";
+    }
+}
