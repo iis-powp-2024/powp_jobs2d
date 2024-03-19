80228250b8776010dc6fd5633762ee9d33637503
DariuszMielczarek <240752@edu.p.lodz.pl>
2024-03-19 18:53:50 +0100

    Wprowadzenie klasy ComplexCommand

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -1,8 +1,30 @@
 package edu.kis.powp.command;
 
-public class ComplexCommand implements DriverCommand{
-    //implementuje DriverCommand i agregacja ComplexCommand do 0..* DriverCommand
-    public void execute() {
+import java.util.ArrayList;
 
+public class ComplexCommand implements DriverCommand {
+    ArrayList<DriverCommand> commands = new ArrayList<>();
+
+    public void addCommand(DriverCommand command) {
+        this.commands.add(command);
+    }
+
+    public void removeCommand(int pos) {
+        this.commands.remove(pos);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        for (DriverCommand command : this.commands) {
+            res.append(command.toString());
+        }
+        return res.toString();
+    }
+
+    @Override
+    public void execute() {
+        for (DriverCommand command : this.commands)
+            command.execute();
     }
 }
