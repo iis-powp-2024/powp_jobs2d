d6cc3cbf28113520397974418019b990762ebabf
Piotr Jasinski <piotr.jasinski2002@gmail.com>
2024-03-20 21:04:10 +0100

    wstępne zaprojektowanie klasy ComplexCommand

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -1,9 +1,27 @@
 package edu.kis.powp.command;
 
 import java.sql.Driver;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand {
-    public List<DriverCommand> commands;
+    private List<DriverCommand> commands;
+
+    public ComplexCommand(){
+        this.commands = new ArrayList<>();
+    }
+
+    public void addCommand(DriverCommand command){
+        this.commands.add(command);
+    }
+
+
+    @Override
+    public void execute(){
+        for (DriverCommand command : this.commands){
+            command.execute();
+        }
+    }
+
 }
