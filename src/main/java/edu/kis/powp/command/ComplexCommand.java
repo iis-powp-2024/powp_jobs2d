a2cfdf72b2c1f3e05cd418cad88eb9e922315fd7
Lecrut <filip.lecrut12@gmail.com>
2024-03-20 21:01:00 +0100

    dodanie klasy ComplexCommand

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.command;
+import java.util.ArrayList;
+import java.util.List;
+public class ComplexCommand implements DriverCommand {
+    private List<DriverCommand> commands = new ArrayList<>();
+
+    ComplexCommand(){
+    }
+
+    public void addCommand(DriverCommand command){
+        this.commands.add(command);
+    }
+
+
+    @Override
+    public void execute(){
+        for (DriverCommand command : this.commands){
+            command.execute();
+        }
+    }
+}
