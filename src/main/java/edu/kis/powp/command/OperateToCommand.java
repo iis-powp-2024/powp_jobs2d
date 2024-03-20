fa1f2a7e5a9b7bb93e0a950271688747e2b7da05
Lecrut <filip.lecrut12@gmail.com>
2024-03-20 20:36:43 +0100

    implementacja DriverCommand

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -5,15 +5,16 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class OperateToCommand implements DriverCommand {
 
     private int x, y;
+    private Job2dDriver driver;
 
-    public OperateToCommand(int posX, int posY) {
-        super();
+    public OperateToCommand(int posX, int posY, Job2dDriver driver) {
         this.x = posX;
         this.y = posY;
+        this.driver = driver;
     }
 
     @Override
-    public void execute(Job2dDriver driver) {
-        driver.operateTo(x, y);
+    public void execute() {
+        this.driver.operateTo(x, y);
     }
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -5,16 +5,17 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public class SetPositionCommand implements DriverCommand {
 
     private int x, y;
+    private Job2dDriver driver;
 
-    public SetPositionCommand(int posX, int posY) {
-        super();
+    public SetPositionCommand(int posX, int posY, Job2dDriver driver) {
         this.x = posX;
         this.y = posY;
+        this.driver = driver;
     }
 
     @Override
-    public void execute(Job2dDriver driver) {
-        driver.setPosition(x, y);
+    public void execute() {
+        this.driver.setPosition(x, y);
     }
 
 }
