589f7864008acad3dc853b19f247550d62aa646a
juliusz brodnicki <juliusbrodnicki@gmail.com>
2024-03-21 13:48:29 +0100

    Added triangle method to ShapeFactory

--- src/main/java/edu/kis/powp/jobs2d/command/ShapeFactory.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ShapeFactory.java
@@ -20,6 +20,14 @@ public class ShapeFactory {
     }
 
     public static ComplexCommand triangle(Job2dDriver driver){
-
+        DriverCommand driverCommand1 = new SetPositionCommand(0,0);
+        DriverCommand driverCommand2 = new OperateToCommand(100,100);
+        DriverCommand driverCommand3 = new OperateToCommand(100,0);
+        DriverCommand driverCommand4 = new OperateToCommand(0,0);
+        List<DriverCommand> driverCommands = Arrays.asList(driverCommand1,
+                driverCommand2, driverCommand3, driverCommand4);
+        ComplexCommand complexCommand = new ComplexCommand(driverCommands);
+        complexCommand.setDriver(driver);
+        return complexCommand;
     }
 }
