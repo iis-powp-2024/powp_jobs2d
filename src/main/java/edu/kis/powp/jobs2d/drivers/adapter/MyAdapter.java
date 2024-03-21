0875b087b430056d063253334cd846d994fc55f9
Weronika Kretowicz <235903@edu.p.lodz.pl>
2024-03-21 22:22:45 +0100

    Removing inheritance from MyAdapter.java

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -9,11 +9,10 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class MyAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
 	public MyAdapter() {
-		super();
 	}
 
 	@Override
