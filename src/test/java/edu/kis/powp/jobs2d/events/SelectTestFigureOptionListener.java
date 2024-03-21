44f85969d6ca8068288045efc0b979b74e09f157
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-03-21 21:05:51 +0100

    Implementation of test selecting for listener

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.drivers.command.ComplexFactoryCommand;
+import edu.kis.powp.jobs2d.drivers.command.DriverCommand;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
@@ -18,12 +20,21 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		DriverCommand command;
 		switch (testNumber) {
 			case 1:
 				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
 				break;
 			case 2:
 				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+				break;
+			case 3:
+				command = ComplexFactoryCommand.rectangle(driverManager.getCurrentDriver(), 0, 0, 100, 100);
+				command.execute();
+				break;
+			case 4:
+				command = ComplexFactoryCommand.circle(driverManager.getCurrentDriver(), 0, 0, 50);
+				command.execute();
 				break;
 		}
 	}
