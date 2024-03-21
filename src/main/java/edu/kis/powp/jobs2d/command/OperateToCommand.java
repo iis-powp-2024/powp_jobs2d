2fccd39d83c62a97e3c36cdd877a970e1f0d57a7
juliusz brodnicki <juliusbrodnicki@gmail.com>
2024-03-21 12:49:11 +0100

    Delete blank space

--- src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -2,7 +2,7 @@ package edu.kis.powp.jobs2d.command;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
-public class OperateToCommand implements DriverCommand {
+public class OperateToCommand implements DriverCommand{
     private Job2dDriver driver;
     private int x;
     private int y;
