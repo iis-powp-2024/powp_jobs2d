38c6cfd5f284b296813be2015dce0db8da70641b
Igor Joński <jonskiigor@gmail.com>
2024-03-21 12:49:34 +0100

    Add Job2dDriver to command classes, implement override of execute() function

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,17 +1,21 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
 
+    final private Job2dDriver driver;
     final private int x;
     final private int y;
 
-    public OperateToCommand(int x, int y) {
+    public OperateToCommand(Job2dDriver driver, int x, int y) {
+        this.driver = driver;
         this.x = x;
         this.y = y;
     }
 
     @Override
     public void execute() {
-
+        driver.operateTo(x, y);
     }
 }
