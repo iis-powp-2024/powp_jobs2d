7ead9a41a79b627594781a2dbfa4dea67735899b
Igor Joński <jonskiigor@gmail.com>
2024-03-21 13:07:55 +0100

    Answer question about design patterns in javadoc of ComplexCommand.java file

--- src/main/java/edu/kis/powp/command/ComplexCommand.java
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -2,6 +2,11 @@ package edu.kis.powp.command;
 
 import java.util.List;
 
+/**
+ * Complex command class.
+ * In command package, we use command and composite design patterns.
+ */
+
 public class ComplexCommand implements DriverCommand {
     final private List<DriverCommand> driverCommands;
 
