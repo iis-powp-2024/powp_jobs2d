83447704f52bf73982227d596aa4b39b4915c520
juliusz brodnicki <juliusbrodnicki@gmail.com>
2024-03-21 13:34:13 +0100

    Replace interface Shape with ShapeFactory

--- src/main/java/edu/kis/powp/jobs2d/command/Shape.java
+++
@@ -1,7 +0,0 @@
-package edu.kis.powp.jobs2d.command;
-
-import java.util.List;
-
-public interface Shape {
-    List<DriverCommand> createShape();
-}
---
+++ src/main/java/edu/kis/powp/jobs2d/command/ShapeFactory.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.jobs2d.command;
+
+public class ShapeFactory {
+    public static void Rectangle(){
+
+    }
+
+    public static void Triangle(){
+
+    }
+}
