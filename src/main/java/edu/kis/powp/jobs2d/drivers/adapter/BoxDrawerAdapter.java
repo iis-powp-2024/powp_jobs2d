d33f4351853cabbb0e0fecf34bfcadd11807e8c8
Matenar <240736@edu.p.lodz.pl>
2024-03-28 06:11:46 +0100

    Removed unnecessary inheritance

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/BoxDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/BoxDrawerAdapter.java
@@ -10,7 +10,7 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class BoxDrawerAdapter extends DrawPanelController  implements Job2dDriver {
+public class BoxDrawerAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
 	private final DrawPanelController drawerController;
 
